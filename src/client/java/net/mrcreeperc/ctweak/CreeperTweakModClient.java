42efedfb297225d81508a5cb2857dbea7c5fd042
Imrasts <olgaakshidi.ru@gmail.com>
2023-08-27 18:58:50 +0300

    Create CreeperTweaks!

---
+++ src/client/java/net/mrcreeperc/ctweak/CreeperTweakModClient.java
@@ -0,0 +1,10 @@
+package net.mrcreeperc.ctweak;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreeperTweakModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
